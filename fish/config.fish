7a66f1216c535bb2d29085212ba32a44b4a5edcf
zahdr <f.wischendorff@posteo.de>
2024-02-24 14:39:46 +0100

    added fish for testing purpose

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# disable annoying greeting message
+set -g fish_greeting
+
+# some aliases
+alias l="ls -lh"
+alias ll="ls -lah"
+alias lg="lazygit"
+alias exercism="~/bin/exercism"
