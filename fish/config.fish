b616b1864d8868ef8d9d9d4b860bedd46028ea68
zahdr <f.wischendorff@posteo.de>
2024-02-24 18:55:14 +0100

    added basic zettelkasten and improved cd commands

--- fish/config.fish
+++ fish/config.fish
@@ -12,12 +12,13 @@ fish_add_path $HOME/scripts
 # exports
 export DOTFILES="$HOME/dotfiles"
 export SCRIPTS="$HOME/dotfiles/scripts"
+export ZETTELKASTEN="$HOME/zettelkasten"
 
 # some aliases
 alias l="ls -lh"
 alias ll="ls -lah"
 alias reload="source $HOME/.config/fish/config.fish"
-alias dotfiles="cd $HOME/dotfiles"
-alias scripts="cd $HOME/dotfiles/scripts"
+alias dotfiles="cd $DOTFILES"
+alias scripts="cd $SCRIPTS"
+alias zettelkasten="cd $ZETTELKASTEN"
 alias lg="lazygit"
-alias exercism="$HOME/bin/exercism"
