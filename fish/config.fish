b6a819a560595e6532f98f5c7d3256f5308866bd
zahdr <f.wischendorff@posteo.de>
2024-02-24 16:49:13 +0100

    step by step update

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,19 @@ end
 # disable annoying greeting message
 set -g fish_greeting
 
+# extend path
+fish_add_path $HOME/bin
+fish_add_path $HOME/scripts
+
+# exports
+export DOTFILES="$HOME/dotfiles"
+export SCRIPTS="$HOME/dotfiles/scripts"
+
 # some aliases
 alias l="ls -lh"
 alias ll="ls -lah"
+alias reload="source $HOME/.config/fish/config.fish"
+alias dotfiles="cd $HOME/dotfiles"
+alias scripts="cd $HOME/dotfiles/scripts"
 alias lg="lazygit"
-alias exercism="~/bin/exercism"
+alias exercism="$HOME/bin/exercism"
--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+set -e 
+
+if [[ ":$PATH:" != *":$HOME/scripts:"* ]]; then
+	export PATH="$PATH:$HOME/scripts"
+fi
+
+if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
+	export PATH="$PATH:$HOME/bin"
+fi
+
+ln -sf "$PWD/scripts" "$HOME/scripts"
+
 for i in fish newsboat tmux vim ; do
 	cd $i && ./setup.sh
 	cd ..
