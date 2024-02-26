c1279843cb13484ec5622e1ea02dc5c7b4fde2a8
zahdr <f.wischendorff@posteo.de>
2024-02-26 09:52:15 +0100

    added if statement for dir checking

--- setup.sh
+++ setup.sh
@@ -10,7 +10,9 @@ if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
 	export PATH="$PATH:$HOME/bin"
 fi
 
-ln -sf "$PWD/scripts" "$HOME/scripts"
+if [[ ! -L "$HOME/scripts" ]]; then
+    ln -sf "$PWD/scripts" "$HOME/scripts"
+fi
 
 for i in fish newsboat tmux vim ; do
 	cd $i && ./setup.sh
