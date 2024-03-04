c2d37b4e93cdb9b1940cef71bef822ba68761e83
zahdr <f.wischendorff@posteo.de>
2024-03-04 08:16:25 +0100

    added dir creation

--- newsboat/setup.sh
+++ newsboat/setup.sh
@@ -2,6 +2,8 @@
 
 ! type newsboat > /dev/null 2>&1 && echo "newsboat not found" && exit 127
 
+mkdir -p "$HOME/.config/newsboat"
+
 if [ "$(uname)" == "Darwin" ]; then
 	ln -sf "$PWD/config_mac" "$HOME/.newsboat/config"
 	ln -sf "$PWD/urls" "$HOME/.newsboat/urls"
