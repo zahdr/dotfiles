15cac6b30972e620906daec373325f194931e4a3
zahdr <f.wischendorff@posteo.de>
2024-07-19 10:14:00 +0200

    moved work related path to different configs

--- fish/config.fish
+++ fish/config.fish
@@ -11,13 +11,11 @@ bind \cf "source $HOME/scripts/fzfcd; commandline -f repaint"
 # extend path
 fish_add_path $HOME/bin
 fish_add_path $HOME/scripts
-fish_add_path $HOME/scripts-work
 fish_add_path /opt/homebrew/bin
 
 # exports
 export DOTFILES="$HOME/dotfiles"
 export SCRIPTS="$HOME/dotfiles/scripts"
-export SCRIPTSWORK="$HOME/dotfiles/scripts-work"
 export ZETTELKASTEN="$HOME/zettelkasten"
 
 # some aliases
@@ -27,7 +25,6 @@ alias ll="ls -lah"
 alias reload="source $HOME/.config/fish/config.fish"
 alias dotfiles="cd $DOTFILES"
 alias scripts="cd $SCRIPTS"
-alias scriptswork="cd $SCRIPTSWORK"
 alias zettelkasten="cd $ZETTELKASTEN"
 alias lg="lazygit"
 alias todo="vim $DOTFILES/todo.md"
