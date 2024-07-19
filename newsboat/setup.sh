cc83c11d0fbac1bf330a83df0178a4898cf8f144
zahdr <f.wischendorff@posteo.de>
2024-07-19 14:49:37 +0200

    change setup alias and pathing

--- newsboat/setup.sh
+++ newsboat/setup.sh
@@ -4,12 +4,12 @@
 
 mkdir -p "$HOME/.config/newsboat"
 
+ln -sf "$PWD/urls" "$HOME/.config/newsboat/urls"
+
 if [ "$(uname)" == "Darwin" ]; then
-	ln -sf "$PWD/config_mac" "$HOME/.newsboat/config"
-	ln -sf "$PWD/urls" "$HOME/.newsboat/urls"
+	ln -sf "$PWD/config_mac" "$HOME/.config/newsboat/config"
 elif [ "$(uname)" == "Linux" ]; then
 	ln -sf "$PWD/config_linux" "$HOME/.config/newsboat/config"
-	ln -sf "$PWD/urls" "$HOME/.config/newsboat/urls"
 else
 	echo "error in linking newsboat files"
 fi
