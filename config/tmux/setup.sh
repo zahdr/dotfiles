c0e792f2503e9eaafd055add4497ff98bf012d4c
zahdr <f.wischendorff@posteo.de>
2024-08-14 22:09:27 +0200

    fix tmux config location

--- config/tmux/setup.sh
+++ config/tmux/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [[ "$(command -v tmux)" ]]; then
-    mkdir -p "$HOME/.config/tmux"
-    ln -sf "$PWD/tmux.conf" "$HOME/.config/tmux/.tmux.conf"
+    ln -sf "$PWD/tmux.conf" "$HOME/.tmux.conf"
     echo "Created and linked config for tmux"
 else
     echo "WARNING! Could not find 'tmux'"
