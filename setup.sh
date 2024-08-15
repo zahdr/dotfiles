8452f5c426a355bd4f2a958c73f7653cfe5ba93c
zahdr <f.wischendorff@posteo.de>
2024-08-15 17:25:46 +0200

    add some output

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ install-scripts/create-links.sh
 install-scripts/install-fonts.sh
 
 echo ""
+echo "-----------------------------------------------"
+echo "INFO - If you want to set secrets use 'secrets'"
+echo "-----------------------------------------------"
+echo ""
 echo ".------------."
 echo "| SETUP DONE |"
 echo ".------------."
