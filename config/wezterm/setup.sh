454fc03a53363fdb943b944b6fdf96a95a6a37ed
zahdr <f.wischendorff@posteo.de>
2024-10-14 11:14:21 +0200

    replaced wezterm by alacritty

--- config/wezterm/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$(command -v wezterm)" ]]; then
-    mkdir -p "$HOME/.config/wezterm"
-    ln -sf "$PWD/wezterm.lua" "$HOME/.config/wezterm/wezterm.lua"
-    echo "Created and linked config for wezterm"
-else
-    echo "WARNING! Could not find 'wezterm'"
-fi
